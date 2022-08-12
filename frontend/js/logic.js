25bba5204279c3289c7d80fc0b45009858cef76c
Saeed99Madi <saed.zooro@gmail.com>
2022-08-12 19:34:53 +0300

    write search functionality dom edit search function and write function return the user who neaded edit in home page write script html elements edit in style css related in #7

--- frontend/js/logic.js
+++ frontend/js/logic.js
@@ -1,5 +1,13 @@
 const searchCharacter = (charactersArr, str) => {
-  return charactersArr.filter(item => {
-    return item.name.includes(str);
-  })
-}
+  return charactersArr.filter((ele) => {
+    return ele.name.toLowerCase().includes(str.toLowerCase());
+  });
+};
+
+const searchUser = (charactersArr, str) => {
+  const result = charactersArr.filter((ele) => {
+    return ele.id === str;
+  });
+
+  return result[0];
+};
