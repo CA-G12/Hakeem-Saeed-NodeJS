d8653e62efc2aced6be4b98ffd8a144c5aa4e08e
Saeed99Madi <saed.zooro@gmail.com>
2022-08-13 15:00:31 +0300

    a lot editing becouse eslint and try to fix deploy proplem related to #29

--- backend/handler.js
+++ backend/handler.js
@@ -1,13 +1,13 @@
-const fs = require("fs");
+const fs = require('fs');
 
 const handler = (res, filePath, contentType) => {
-  fs.readFile(filePath, "utf-8", (err, data) => {
+  fs.readFile(filePath, 'utf-8', (err, data) => {
     if (err) {
       console.log(err);
-      res.writeHead(500, { "Content-Type": "text/html" });
-      res.end("<h1>Internal Server Error</h1>");
+      res.writeHead(500, { 'Content-Type': 'text/html' });
+      res.end('<h1>Internal Server Error</h1>');
     } else {
-      res.writeHead(200, { "Content-Type": `${contentType}` });
+      res.writeHead(200, { 'Content-Type': `${contentType}` });
       res.end(data);
     }
   });
