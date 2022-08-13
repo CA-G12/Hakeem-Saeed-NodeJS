f0ba6ec14894e69d8b4701b88913dc1afcb266b9
Saeed99Madi <saed.zooro@gmail.com>
2022-08-13 15:03:00 +0300

    Revert "a lot editing becouse eslint and try to fix deploy proplem"
    This reverts commit d8653e62efc2aced6be4b98ffd8a144c5aa4e08e.

--- backend/routes.js
+++ backend/routes.js
@@ -1,60 +1,61 @@
-const path = require('path');
-const handler = require('./handler');
+const path = require("path");
+const handler = require("./handler");
 
 const router = (req, res) => {
   const endPoint = req.url;
   switch (endPoint) {
-    case '/': {
+    case "/": {
       const filePath = path.join(
         __dirname,
-        '..',
-        'frontend',
-        'pages',
-        'home.html',
+        "..",
+        "frontend",
+        "pages",
+        "home.html"
       );
-      handler(res, filePath, 'text/html');
+      handler(res, filePath, "text/html");
 
       break;
     }
-    case '/reset.css': {
-      const filePath = path.join(__dirname, '..', 'frontend', 'reset.css');
-      handler(res, filePath, 'text/css');
+    case "/reset.css": {
+      const filePath = path.join(__dirname, "..", "frontend", "reset.css");
+      handler(res, filePath, "text/css");
 
       break;
     }
-    case '/style.css': {
-      const filePath = path.join(__dirname, '..', 'frontend', 'style.css');
-      handler(res, filePath, 'text/css');
+    case "/style.css": {
+      const filePath = path.join(__dirname, "..", "frontend", "style.css");
+      handler(res, filePath, "text/css");
 
       break;
     }
-    case '/js/dom.js': {
-      const filePath = path.join(__dirname, '..', 'frontend', 'js', 'dom.js');
-      handler(res, filePath, 'text/javascipt');
+    case "/js/dom.js": {
+      const filePath = path.join(__dirname, "..", "frontend", "js", "dom.js");
+      handler(res, filePath, "text/javascipt");
 
       break;
     }
-    case '/js/logic.js': {
-      const filePath = path.join(__dirname, '..', 'frontend', 'js', 'logic.js');
-      handler(res, filePath, 'text/javascipt');
+    case "/js/logic.js": {
+      const filePath = path.join(__dirname, "..", "frontend", "js", "logic.js");
+      handler(res, filePath, "text/javascipt");
 
       break;
     }
-    case '/js/fetch.js': {
-      const filePath = path.join(__dirname, '..', 'frontend', 'js', 'fetch.js');
+    case "/js/fetch.js": {
+      const filePath = path.join(__dirname, "..", "frontend", "js", "fetch.js");
 
-      handler(res, filePath, 'text/javascipt');
+      handler(res, filePath, "text/javascipt");
 
       break;
     }
-    case '/users': {
-      const filePath = path.join(__dirname, 'data.json');
-      handler(res, filePath, 'application/json');
+    case "/users": {
+      const filePath = path.join(__dirname, "data.json");
+      handler(res, filePath, "application/json");
       break;
     }
+
     default: {
-      res.writeHead(404, { 'Content-Type': 'text/html' });
-      res.end('<h1>Not Found</h1>');
+      res.writeHead(404, { "Content-Type": "text/html" });
+      res.end("<h1>Not Found</h1>");
       break;
     }
   }
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 const http = require('http');
 
-const routes = require('./routes');
+const routes = require('./routes')
 
 const server = http.createServer(routes);
 const PORT = 8080;
 
 server.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server is listening on port ${PORT}`)
 });
